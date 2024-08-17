fbde87e340684a4e8321019e759df5740591d93c
MaxThePixeI <mailhelp08@gmail.com>
2024-08-17 22:41:17 +0200

    Fix
    Fix the command

--- ommand.py
+++ ommand.py
@@ -1,7 +1,1 @@
-import sublime
-import sublime_plugin
-
-
-class ExampleCommand(sublime_plugin.TextCommand):
-	def run(self, edit):
-		self.view.insert(edit, 0, "Hello world im Max an I from Denmerk in a small city callet Føvling and Im 15")
+print("Hej")
